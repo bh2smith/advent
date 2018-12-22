9de07d50b06edb24e61bbc07a5fe56a1b317513f
bh2smith <bh2smith@gmail.com>
2018-12-22 22:31:42 +0100

    Day 1: semi-golfed

--- day01_sol.py
+++ day01_sol.py
@@ -1,19 +1,12 @@
 if __name__ == '__main__':
-    with open('data/day01') as f:
-        frequencies = []
-        t = f.readlines()
-        for line in t:
-            frequencies.append(int(line.strip()))
+    freaks = list(map(lambda l: int(l.strip()), open('data/day01').readlines()))
 
-    # Part 1
-    print(sum(frequencies))
+    print("Part1:", sum(freaks))
 
-    # Part 2
-    f, i = 0, 0
-    seen = set()
+    f, i, seen = 0, 0, set()
     while f not in seen:
         seen.add(f)
-        f += frequencies[i]
+        f += freaks[i]
         i += 1
-        i %= len(frequencies)
-    print(f)
+        i %= len(freaks)
+    print("Part2:", f)
